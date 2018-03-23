bfa612fc1b2579ec76a2f714e275ec97daa45089
Mistrfilda <filda.kuchar@seznam.cz>
2018-03-23 20:01:55 +0100

    Base presenter

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -9,5 +9,38 @@ use Nette\Application\UI\Presenter;
 
 abstract class BasePresenter extends Presenter
 {
+	public function startup()
+	{
+		parent::startup();
+		$this->getTemplate()->menu = $this->getMenu();
+	}
 
+
+	private function getMenu()
+	{
+		return [
+			'home' => [
+				'label' => 'Home',
+				'icon'  => 'user',
+				'link'  => 'Homepage:Default',
+			],
+			'system' => [
+				'label' => 'System',
+				'icon' => 'database',
+				'link' => 'System:default',
+				'submenu' => [
+					'users' => [
+						'label' => 'Users',
+						'icon' => 'users',
+						'link' => 'System:Users'
+					],
+					'rights' => [
+						'label' => 'Rights',
+						'icon' => 'gavel',
+						'link' => 'System:Rights'
+					],
+				],
+			],
+		];
+	}
 }
