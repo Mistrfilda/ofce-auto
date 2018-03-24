9d4294208f08c72ae79ceafd3d80b9827b4929bd
Mistrfilda <filda.kuchar@seznam.cz>
2018-03-24 17:02:05 +0100

    Logged user

--- app/presenters/SecurePresenter.php
+++ app/presenters/SecurePresenter.php
@@ -4,13 +4,36 @@
 namespace App\Presenters;
 
 
+use App\Model\Database\Entity\User;
+use App\Model\UserModel;
+
+
 class SecurePresenter extends BasePresenter
 {
+	/** @var User */
+	private $loggedUser;
+
+	/** @var  UserModel */
+	protected $userModel;
+
+	public function injectUserModel(UserModel $userModel)
+	{
+		$this->userModel = $userModel;
+	}
+
 	public function startup()
 	{
 		parent::startup();
 		if (!$this->user->isLoggedIn()) {
 			$this->redirect('Login:Login', ['backlink' => $this->storeRequest()]);
+		} else {
+			$this->loggedUser = $this->userModel->getData($this->getUser()->getId());
 		}
+	}
+
+	public function beforeRender()
+	{
+		parent::beforeRender();
+		$this->getTemplate()->loggedUser = $this->loggedUser;
 	}
 }
